9c67468ea82d342523f8fa201462aa14e71ac877
YeonJun Jo <micayel1219@gmail.com>
2022-07-28 10:24:38 +0900

    junit, assertJ 도입

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+    @Test
+    void 테스트_assertThat() {
+        int rs = 10 + 20;
+        assertThat(rs).isEqualTo(30);
+    }
+}
