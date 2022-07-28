c913b4626f2f9506a7317b638b656b4205626c6b
YeonJun Jo <micayel1219@gmail.com>
2022-07-28 14:40:37 +0900

    rq.json 구현

--- src/main/java/com/ll/exam/article/ArticleController.java
+++ src/main/java/com/ll/exam/article/ArticleController.java
@@ -130,8 +130,6 @@ public class ArticleController {
     public void getArticles(Rq rq) {
         List<ArticleDto> articleDtos = articleService.findAll();
 
-        String jsonStr = Ut.json.toStr(articleDtos, "");
-
-        rq.println(jsonStr);
+        rq.json(articleDtos);
     }
 }
